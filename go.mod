ace4a819b058848387a86265c52c52c2ccad02ee
jon-castro <jcastrogarcia@gmail.com>
2025-08-24 22:17:41 -0500

    Answers

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/jon-castro/gator-golang
 go 1.25.0
 
 require (
-	github.com/google/uuid v1.6.0
-	github.com/lib/pq v1.10.9
+	github.com/google/uuid v1.3.0
+	github.com/lib/pq v1.10.7
 )
--- handler_feed.go
+++ handler_feed.go
@@ -9,12 +9,7 @@ import (
 	"github.com/jon-castro/gator-golang/internal/database"
 )
 
-func handlerAddFeed(s *state, cmd command) error {
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
-	if err != nil {
-		return err
-	}
-
+func handlerAddFeed(s *state, cmd command, user database.User) error {
 	if len(cmd.Args) != 2 {
 		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
 	}
@@ -85,4 +80,5 @@ func printFeed(feed database.Feed, user database.User) {
 	fmt.Printf("* Name:          %s\n", feed.Name)
 	fmt.Printf("* URL:           %s\n", feed.Url)
 	fmt.Printf("* User:          %s\n", user.Name)
+	fmt.Printf("* LastFetchedAt: %v\n", feed.LastFetchedAt.Time)
 }
